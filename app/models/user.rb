7a800b20814fd4479d2c6e5858354fc7d8eb3a8c
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2024-01-15 17:26:50 +0900

    Homepage controller added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :nickname, presence: true, uniqueness: true, length: { maximum: 20 }
 end
