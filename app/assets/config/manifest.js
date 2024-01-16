4762aa0070c18e5fba7c2aebfa161ff347bc05e0
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2024-01-16 16:50:51 +0900

    Added bootstrap

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link_tree ../images
+//= link popper.js
+//= link bootstrap.min.js
