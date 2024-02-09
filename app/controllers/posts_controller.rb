0f4ae57b55d510a515686d89711b09af4ae3148a
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2024-02-09 19:02:10 +0900

    added video and thumbnail to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,6 +65,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description)
+      params.require(:post).permit(:title, :description, :video, :thumbnail)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  has_one_attached :thumbnail
+  has_one_attached :video
 end
